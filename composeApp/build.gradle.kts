3ea59cc38885e88ac45f02c751352b72c024a2b6
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2024-10-28 06:00:30 +0900

    add androidx datastore dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -41,6 +41,8 @@ kotlin {
                 implementation(compose.components.uiToolingPreview)
                 implementation(libs.androidx.lifecycle.viewmodel)
                 implementation(libs.androidx.lifecycle.runtime.compose)
+                // AndroidX DataStore
+                implementation(libs.androidx.datastore.preferences.core)    // preferences datastore¬
             }
         }
         val androidMain by getting {
